7b194c2bba7e3d53ee205a6a4d94654b0b4c1109
Alexandre Gayer da Silva <alexandregs119@hotmail.com>
2023-12-26 14:50:34 -0300

    fundamentos Kotlin

---
+++ src/main/kotlin/fundamentos/collections.kt
@@ -0,0 +1,38 @@
+package org.fundamentos.fundamentos
+
+fun main(){
+//    var lista = listOf(1,2,3,4,6);
+//    for (numero in lista){
+//        println(numero);
+//    }
+
+//    println(lista[0])
+//    println(lista.get(0))
+//    println(lista.size)
+//    println(lista.indexOf(6))
+
+//    var lista = mutableListOf(1,2,3,5,6,8)
+
+//    lista.add(9)
+//    lista.remove(1)
+//    lista.removeAt(index = 2)
+//    lista[3] = 20
+//
+//    println(lista)
+
+//    lista.sort()
+//    lista.shuffle()
+//    println(lista)
+
+
+    //nao mostra numeros duplicados - setOf
+//
+//    var setNumeros = setOf(1,2,3,3,4)
+//    println(setNumeros)
+
+var mapNomeIdade = mutableMapOf("Gustavo" to 24, "Alexandre" to 32)
+    println(mapNomeIdade)
+
+
+
+}
---
+++ src/main/kotlin/fundamentos/estruturawhen.kt
@@ -0,0 +1,13 @@
+package org.fundamentos.fundamentos
+
+
+fun main(){
+    val x = 10;
+
+    when(x){
+        5 -> println("x == 5");
+        8 -> println("x == 8");
+        else -> println("Numero nao mapeado")
+    }
+
+}
